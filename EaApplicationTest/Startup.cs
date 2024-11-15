b4612514fd21c0e15380d340402244ed173ee038
tara5401 <taramani5401@gmail.com>
2024-11-15 10:50:51 +0000

    IEmployeePage and ILoginPage added to scope

--- EaApplicationTest/Startup.cs
+++ EaApplicationTest/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using EaFramwork.Config;
 using EaFramwork.Driver;
+using EaFramwork.Pages;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EaApplicationTest;
@@ -8,10 +9,14 @@ public class Startup
 {
     public void ConfigureServices(IServiceCollection services) 
     {
-        services.AddSingleton(ConfigReader.ReadConfig);
-        services.AddScoped<IPlaywrightDriver, PlaywrightDriver>();
-        services.AddScoped<IPlaywrightDriverInitializer, PlaywrightDriverInitializer>();
-        
-    
+        services
+            .AddSingleton(ConfigReader.ReadConfig())
+            .AddScoped<IPlaywrightDriver, PlaywrightDriver>()
+            .AddScoped<IPlaywrightDriverInitializer, PlaywrightDriverInitializer>()
+            .AddScoped<IEmployeePage, EmployeePage>()
+            .AddScoped<ILoginPage, LoginPage>();
+
+
+
     }
 }
